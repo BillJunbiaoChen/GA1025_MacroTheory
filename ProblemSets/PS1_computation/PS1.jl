dac0e9b3a0dc9e93d455eb5a3daeab0a80227fcb
junbiaochen <junbiaochen@uchicago.edu>
2025-09-06 22:17:36 -0400

    committed WIP: vis reservation wage

--- ProblemSets/PS1_computation/PS1.jl
+++ ProblemSets/PS1_computation/PS1.jl
@@ -1,7 +1,6 @@
 using Distributions, Plots, Random, Parameters, LinearAlgebra
 
 
-
 T = 50
 β = 0.97
 B = 1
@@ -26,9 +25,26 @@ for j in 2:(J-1)
 end
 
 f_vec[J] = 1 - cdf(beta_dist, 0.5* (w_vec[J] + w_vec[J-1]))
+Ew = sum(w_vec .* f_vec)
+
+# Question 3.8 Compute reservation wages 
+w̄_vec = zeros((T+1))
+w̄_vec[T+1] = c # w̄_T = c 
 
 
+for t in T:-1:1
+    reject_idx = w_vec .< w̄_vec[t+1]
+    p_rej = sum(f_vec[reject_idx])
+    term1 = w̄_vec[t+1] * p_rej
+    term2 = sum(f_vec[reject_idx] .* w_vec[reject_idx])
+    w̄_vec[t] = (c * (1 - β) / (1 - β^(T-t+2))) +
+               (β * (1 - β^(T-t+1)) / (1 - β^(T-t+2)) * (Ew + term1 - term2))
+end
 
+
+# Visualize reservation wages
+plot((1:(T+1)), w̄_vec, xlabel="Time", ylabel="Reservation wage", legend=false)
+savefig("reservation_wages_over_time.png")
 
 
 # Question 3.9 Apply Backward Induction to obtain value functions
@@ -55,3 +71,51 @@ for t in 2:(T+1)
 end
 
 savefig("value_functions_over_time.png")
+
+
+# Find the kink for each year
+v_mat_inc = zeros(J, T+1)
+v_w̄ = zeros(T+1)
+
+for i in 2:J
+    v_mat_inc[i, :] = v_mat[i, :] - v_mat[(i-1), :]
+end
+
+for t in 1:(T+1)
+    idx = sum(v_mat_inc[:, t] .< 1e-10)
+    v_w̄[t] = v_mat[:, t][idx]
+end
+
+
+
+# Combine Value Functions with Reservation Wages
+colors = [color for color in cgrad(:viridis, size(v_mat, 2)).colors]
+
+# Base plot
+plot(
+    w_vec,
+    v_mat[:, 1],
+    color=colors[1],
+    xlabel="Wage",
+    ylabel="Value Function",
+    title="Value Functions and Reservation Wage Over Time",
+    legend=false,
+    titlefont=10, guidefont=8, tickfont=6
+)
+
+
+for t in 2:size(v_mat, 2)
+    plot!(w_vec, v_mat[:, t], color=colors[t], label=false)
+end
+
+
+plot!(w̄_vec, v_w̄, color=:blue, lw=2)
+x_anno = w̄_vec[end]
+y_anno = v_w̄[end]
+annotate!(x_anno, y_anno, text("Reservation Wage at t = T", 8, :blue, :left))
+
+x_anno = w̄_vec[1]
+y_anno = v_w̄[1]
+annotate!(x_anno, y_anno, text("Reservation Wage at t = 1", 8, :blue, :left))
+
+savefig("value_functions_over_time_w_reservation_wage.png")

d09c4e2c8d055ba5e635de0a4861382fd7f18b04
junbiaochen <junbiaochen@uchicago.edu>
2025-09-19 20:46:46 -0400

    updated the Shooting algorithm

--- ProblemSets/PS3/PS3_computation/Cass_Koopman_Shooting.jl
+++ ProblemSets/PS3/PS3_computation/Cass_Koopman_Shooting.jl
@@ -1,6 +1,4 @@
-using Optim, Distributions, Plots, Random, Parameters, LinearAlgebra, Roots, ForwardDiff
-using Interpolations 
-
+using Optim, Distributions, Plots, Random, Parameters, LinearAlgebra
 
 γ = 2 
 δ = 0.05
@@ -14,31 +12,31 @@ function k_end(k0, k1, T)
     k_vec[2] = k1
 
     for t in 3:(T+1)
-        term1 = ((1 - δ) *  k_vec[t-1] + A * k_vec[t-1]^α)
-        term2 = (β^(1/γ)) * (1 - δ + A * α * k_vec[t-1]^(α-1))^(1/γ) * ((1-δ) * k_vec[t-2] + A * k_vec[t-2]^ α - k_vec[t-1])
-        k_vec[t] = term1 + term2
+        term1 = (1 - δ) *  k_vec[t-1] + A * (k_vec[t-1]^α)
+        term2 = (β^(1/γ)) * (1 - δ + A * α * k_vec[t-2]^(α-1))^(1/γ) * ((1-δ) * k_vec[t-2] + A * (k_vec[t-2]^α) - k_vec[t-1])
+        @assert (term1 - term2) >= 0 "Error: Capital should be non-negative"
+        k_vec[t] = term1 - term2
     end
-    return k_vec
-end
 
-
-function TVC_val(k_vec)
-    T = length(k_vec) - 1
-    term1 = ((1 - δ) * k_vec[end-1] + A * k_vec[end - 1]^α - k_vec[end])^(-γ)
-    term2 = 1 - δ + A * α * k_vec[end-1]^(α - 1)
-    val = β^T * term1 * term2 * k_vec[end-1]
-    return val / 1e10
+    tvc_term1_vec = ((1 - δ) .* k_vec[1:end-1] + A .* (k_vec[1:end - 1] .^ α) - k_vec[2:end]) .^ (-γ)
+    tvc_term2_vec = 1 .- δ .+ A .* α .* (k_vec[1:end-1] .^ (α - 1))
+    tvc_vec = β .^ (1:T) .* tvc_term1_vec .* tvc_term2_vec .* k_vec[1:end-1]
+    return k_vec, tvc_vec
 end
 
 # Use a solver to find optimal k1 given k0 
-k0 = 1
-T = 500
+k0 = 0.1
+T = 200
 
 function wrapper(k1)
-    k_vec_guess = k_end(k0, k1, T)
-    obj = TVC_val(k_vec_guess)
-    return obj 
+    k_vec_guess, tvc_vec = k_end(k0, k1, T)
+    return tvc_vec[end] 
 end
 
-# Use BFGS to find the minimum, set tol = 1e-3
+# Use L-BFGS to find the minimum, set tol = 1e-3
+result = optimize(wrapper, 1, 100, L-BFGS(), 
+    Optim.Options(g_tol = 1e-2, show_every = 50)
+)
 
+println("Optimal k1: ", result.minimizer)
+println("Minimum value: ", result.minimum)
